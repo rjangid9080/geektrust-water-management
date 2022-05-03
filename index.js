68fbcf4b14ec7cc7db0948ab2547d6bba7b6171f
rjangid9080 <grut.rjangid9080@gmail.com>
2022-05-03 18:26:37 +0530

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let consumptionOfWater;
+const fs = require("fs");
+
 let costOfAllotedWater;
+let totalGuests = 0;
+let waterConsumptionByGuests = 0;
 
 let allotmentOfWater = (typeOfApartment, corporationWater, borewellWater) => {
+  let consumptionOfWater;
   let peopleInApartment;
   if (typeOfApartment === 2) {
     peopleInApartment = 3;
@@ -10,7 +14,7 @@ let allotmentOfWater = (typeOfApartment, corporationWater, borewellWater) => {
     peopleInApartment = 5;
     consumptionOfWater = peopleInApartment * typeOfApartment * 10 * 30;
   } else {
-    console.log("Only 2 BHK and 3 BHK are available");
+    return console.log("Only 2 BHK and 3 BHK are available");
   }
   costOfAllotedWater =
     (consumptionOfWater / (corporationWater + borewellWater)) *
@@ -18,10 +22,9 @@ let allotmentOfWater = (typeOfApartment, corporationWater, borewellWater) => {
 };
 
 let addGuest = (guests) => {
-  let totalGuests = 0;
   totalGuests += guests;
   waterConsumptionByGuests = totalGuests * 10 * 30;
-  return totalGuests;
+  totalGuests;
 };
 
 let costOfTankerWater = () => {
@@ -35,11 +38,11 @@ let costOfTankerWater = () => {
   }
   if (1501 <= waterConsumptionByGuests <= 3000) {
     costOfWaterByGuests =
-      500 * 2 + 1000 * 3(waterConsumptionByGuests - 1500) * 5;
+      500 * 2 + 1000 * 3 + (waterConsumptionByGuests - 1500) * 5;
   }
   if (waterConsumptionByGuests >= 3001) {
     costOfWaterByGuests =
-      500 * 2 + 1000 * 3 + 1500 * 5(waterConsumptionByGuests - 3000) * 8;
+      500 * 2 + 1000 * 3 + 1500 * 5 + (waterConsumptionByGuests - 3000) * 8;
   }
   return costOfWaterByGuests;
 };
@@ -54,3 +57,24 @@ let totalBill = () => {
   );
   return `${totalWaterConsumed} ${totalCostOfConsumedWater}`;
 };
+
+const data = fs.readFileSync("./inputs/input1.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
+const input = data.split("\n").join("").split("\r");
+
+for (let i = 0; i < input.length; i++) {
+  let type = input[i].split(" ");
+  console.log(type);
+  if (type[0] === "ALLOT_WATER") {
+    let apartment = parseInt(type[1]);
+    let waterRatio = type[2].split(":");
+    let corporation = parseInt(waterRatio[0]);
+    let borewell = parseInt(waterRatio[1]);
+    allotmentOfWater(apartment, corporation, borewell);
+  } else if (type[0] === "ADD_GUESTS") {
+    let guests = type[1];
+    addGuest(guests);
+  }
+}
